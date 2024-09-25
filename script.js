dd0233bcd2eff9427fdd9e35134ac0580008518d
YounceLiang <145736254+YounceLiang@users.noreply.github.com>
2024-09-25 16:21:57 +0800

    Update script.js

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ document.getElementById('customerForm').addEventListener('submit', async functio
     `;
 
     try {
-        const response = await fetch('http://localhost:5000/proxy', {
+        const response = await fetch('https://drymundanequadrant.younceliang.repl.co/proxy', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
